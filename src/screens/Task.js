b0482eebf105ecba6e88d57feedca6000dc976f9
nandinhatnl <fernanda@poppymidia.com.br>
2023-04-20 06:37:51 -0300

    adicionando função deleteTask para o projeto

--- src/screens/Task.js
+++ src/screens/Task.js
@@ -1,5 +1,13 @@
 import React from "react";
-import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native'
+import {
+    View, 
+    Text,
+     StyleSheet, 
+     TouchableWithoutFeedback,
+    TouchableOpacity,
+} from 'react-native'
+
+import {Swipeable, GestureHandlerRootView} from 'react-native-gesture-handler'
 import commonStyles from "../commonStyles";
 import Icon from 'react-native-vector-icons/FontAwesome'
 import moment from "moment";
@@ -12,7 +20,30 @@ export default props =>{
 } :{};
 const date = props.doneAt ? props.doneAt: props.estimateAt
 const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM')
+
+const getRightContent = ()=>{
+    return(
+        <TouchableOpacity style={styles.right} onPress={()=> props.onDelete && props.onDelete(props.id)}>
+            <Icon name="trash" size={30} color={'white'} ></Icon>
+        </TouchableOpacity>
+    )
+}
+const getLeftContent = ()=>{
+    return(
+        <View style={styles.left}>
+            <Icon name="trash" size={30} color={'white'} style={styles.excludeIcon}></Icon>
+            <Text style={styles.excludeText}> Excluir</Text>
+        </View>
+    )
+}
     return (
+        <GestureHandlerRootView>
+
+        <Swipeable 
+        renderRightActions={getRightContent} 
+        renderLeftActions={getLeftContent}
+         onSwipeableLeftOpen={()=> props.onDelete && props.onDelete(props.id)}>
+
         <View style={styles.container}>
             <TouchableWithoutFeedback onPress={()=> props.toggleTask(props.id)}>
 
@@ -27,6 +58,8 @@ const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM')
             </View>
            
         </View>
+        </Swipeable>
+        </GestureHandlerRootView>
     )
 }
 
@@ -56,6 +89,7 @@ const styles = StyleSheet.create({
         borderBottomWidth:1,
         alignItems: 'center',
         paddingVertical:10,
+        backgroundColor: 'white'
     },
     checkContainer:{
         width:"20%",
@@ -92,6 +126,32 @@ const styles = StyleSheet.create({
     },
     date:{
         fontSize:12,
+    },
+    right:{
+        backgroundColor:'red',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        paddingHorizontal:20
+    },
+    left:{
+      backgroundColor: 'red',
+      flexDirection:'row',
+      alignItems:'center',
+      flex:1
+      
+
+    },
+    excludeText:{
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFFF',
+        fontSize: 20,
+        margin:10
+
+    },
+    excludeIcon:{
+        marginLeft: 10,
+
     }
     
 })
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -87,6 +87,10 @@ export default class TaskList extends Component{
         this.setState({tasks,  showAddTask: false}, this.filterTasks)
 
     }
+    deleteTask = id=>{
+        const tasks = this.state.tasks.filter(task => task.id !==id)
+        this.setState({tasks}, this.filterTasks)
+    }
     render(){
         const today = moment().locale('pt-br').format('ddd, D  [de] MMMM [de] YY' )
         return(
@@ -111,7 +115,8 @@ export default class TaskList extends Component{
               <FlatList
                data={this.state.visibleTasks} 
                keyExtractor={item=> `${item.id}`} 
-               renderItem={({item})=> <Task {...item} toggleTask={this.toggleTask}/>}/>
+               renderItem={({item})=> <Task {...item} toggleTask={this.toggleTask}
+               onDelete={this.deleteTask}/>}/>
                
                 </View>
                 <TouchableOpacity style={styles.addButton} activeOpacity={0.9} onPress={()=>this.setState({showAddTask: true})}>
